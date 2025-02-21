a9e804355b878deff20a698bdf31ad8c5ff2a3a3
Victor Mazzola <victormazzola2019@gmail.com>
2025-02-21 09:56:15 -0300

    feat: add HomeScreen with Box layout and styled text

--- app/src/main/java/br/com/fiap/welcomepage/screens/HomeScreen.kt
+++ app/src/main/java/br/com/fiap/welcomepage/screens/HomeScreen.kt
@@ -1,21 +1,48 @@
 package br.com.fiap.welcomepage.screens
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier) {
-    Box {
-        Text(text = "FIAP")
+    Box(
+        modifier = Modifier
+            .background(color = Color(0xff123456))
+            .fillMaxSize(),
+            contentAlignment = Alignment.TopCenter
+    ) {
+        Text(
+            text = "FIAP",
+            fontSize = 32.sp,
+            modifier = Modifier.align(alignment = Alignment.CenterEnd)
+        )
+        Button(onClick = {}) {
+            Text(
+                text = "Click here",
+                fontSize = 38.sp
+
+            )
+        }
     }
 }
 
 
-@Preview
+@Preview(showSystemUi = true)
 @Composable
 private fun HomeScreenPreview() {
-
+    HomeScreen()
 }
