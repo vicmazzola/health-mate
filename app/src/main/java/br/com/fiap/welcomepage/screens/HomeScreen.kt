2d079dd7d05b4344644df28158fd0ab6c3aea000
Victor Mazzola <victormazzola2019@gmail.com>
2025-02-22 10:39:30 -0300

    feat: add image and refactor layout using Column

--- app/src/main/java/br/com/fiap/welcomepage/screens/HomeScreen.kt
+++ app/src/main/java/br/com/fiap/welcomepage/screens/HomeScreen.kt
@@ -1,41 +1,50 @@
 package br.com.fiap.welcomepage.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import br.com.fiap.welcomepage.R
 
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier) {
     Box(
         modifier = Modifier
-            .background(color = Color(0xff123456))
+            .background(color = Color(0xFF5783AF))
             .fillMaxSize(),
-            contentAlignment = Alignment.TopCenter
+        contentAlignment = Alignment.TopCenter
     ) {
-        Text(
-            text = "FIAP",
-            fontSize = 32.sp,
-            modifier = Modifier.align(alignment = Alignment.CenterEnd)
-        )
-        Button(onClick = {}) {
-            Text(
-                text = "Click here",
-                fontSize = 38.sp
-
+        Column(
+            modifier =  Modifier
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Bottom,
+            horizontalAlignment = Alignment.Start
+        ) {
+            Image(
+                painter = painterResource(R.drawable.runner),
+                contentDescription = "Runner Image"
             )
+            Text(
+                text = "FIAP",
+                fontSize = 32.sp,
+            )
+            Button(onClick = {}) {
+                Text(
+                    text = "Click here",
+                    fontSize = 38.sp
+                )
+            }
         }
     }
 }
